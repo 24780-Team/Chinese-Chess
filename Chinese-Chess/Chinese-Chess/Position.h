a58f44432f9f8b5e74bef63a085cfe209318e110
EmperorRay <ruim2@andrew.cmu.edu>
2021-11-12 21:53:56 +0800

    modify position

--- Chinese-Chess/Chinese-Chess/Position.h
+++ Chinese-Chess/Chinese-Chess/Position.h
@@ -1,18 +1,18 @@
 #pragma once
 
+using namespace std;
+
 class Position {
 private:
-    int x;
-    int y;
+	int x;
+	int y;
 
 public:
-    Position(int x, int y) {
-        this->x = x;
-        this->y = y;
-    }
-    int getX() { return x; }
-    int getY() { return y; }
-    bool operator== (const Position otherPos) {
-        return (x == otherPos.x) && (y == otherPos.y);
-    }
+	int getX() { return x; };
+	int getY() { return y; };
+	void setX(int x) { this->x = x; };
+	void setY(int y) { this->y = y; };
+	bool operator== (const Position* otherPos) {
+		return (x == otherPos->x && (y == otherPos->y));
+	}
 };
